69a88fe82bb7a9cbcf6309b9aea7c75f8b6472c7
Rodrigol <rodrigobernahola28@gmail.com>
2026-03-12 17:10:11 -0300

    Form implementation

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const indexRouter = require('./routes/indexRouter.js');
+const path = require("node:path");
+
+const assetsPath = path.join(__dirname, "public");
+const PORT = process.env.PORT || 3000;
+
+//Set
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//Use
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+app.use("/", indexRouter);
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
---
+++ routes/indexRouter.js
@@ -0,0 +1,31 @@
+const { Router } = require("express");
+const indexRouter = Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+indexRouter.get("/", (req, res) => {
+    console.log(messages);
+    res.render("index", { title: "Mini Messageboard", messages: messages })
+})
+
+indexRouter.get("/new", (req, res) => {
+    res.render("form");
+})
+
+indexRouter.post("/new", (req, res) => {
+    console.log(req.body);
+    messages.push({ text: messageText, user: messageUser, added: new Date() });
+})
+
+module.exports = indexRouter;
